57dfb5ccfba1221b3687bdcabc2e4f6e149e6e35
Aryann001 <140709829+Aryann001@users.noreply.github.com>
2023-10-14 16:45:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ dotenv.config({
 });
 
 //Middleware
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({  limit: "50mb", extended: true }));
 app.use(fileUpload());
 app.use(
   cors({

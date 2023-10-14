33635a3110333dcc7062e2961f6eec2fe3dbe6a4
Aryann001 <140709829+Aryann001@users.noreply.github.com>
2023-10-14 17:25:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({  limit: "50mb", extended: true }));
-app.use(fileUpload());
+app.use(fileUpload({useTempFiles: true}));
 app.use(
   cors({
     origin: [process.env.FRONTEND_URI],

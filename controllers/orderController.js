617cfcc79b2d022409ffa673a690fd80ce008c69
Aryann001 <140709829+Aryann001@users.noreply.github.com>
2023-10-17 13:47:03 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -118,7 +118,7 @@ export const updateOrder = catchAsyncErrors(async (req, res, next) => {
 async function updateStock(id, quantity) {
   const product = await Product.findById(id);
 
-  product.Stock -= quantity;
+  product.stock -= quantity;
 
   await product.save({ validateBeforeSave: false });
 }
